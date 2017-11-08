f6d1e55cf649cd2c235d1ddaec7105f6c3bb36da
kkozinski8 <32336818+kkozinski8@users.noreply.github.com>
2017-11-08 16:43:48 +0000

    Revert "Merge branch 'master' of https://github.com/kkozinski8/Team26"
    This reverts commit 591bcf9ac7a4324adb9e214e69ffc80bb313d4bd, reversing
    changes made to 23175812e20a8bbf33a2f5c6ded129c3cfc4669f.

--- website/js/script.js
+++ website/js/script.js
@@ -17,20 +17,4 @@ $(document).ready(function() {
     }
   };
   window.setInterval(checkClassChange, 500);
-  $(window).scroll(function() {
-    $('.history-section').each(function() {
-      var scrollTop = $(window).scrollTop(),
-        elementOffset = $(this).offset().top,
-        distance = (elementOffset - scrollTop),
-        windowHeight = $(window).height(),
-        breakPoint = windowHeight * 0.9;
-
-      if (distance > breakPoint) {
-        $(this).addClass("more-padding");
-      }
-      if (distance < breakPoint) {
-        $(this).removeClass("more-padding");
-      }
-    });
-  });
 })
